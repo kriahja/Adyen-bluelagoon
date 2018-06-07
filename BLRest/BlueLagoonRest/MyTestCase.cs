e5fdf9d686a55c679bf8b9c3dde733bc069b8a79
kristjan <kriahja@gmail.com>
2018-06-07 11:12:57 +0000

    Test test

---
+++ BLRest/BlueLagoonRest/MyTestCase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NUnit.Framework;
+
+namespace BlueLagoonRest
+{
+    [TestFixture]
+    public class MyTestCase
+    {
+
+        MyMathTest math = new MyMathTest();
+
+        [TestCase]
+        public void Add()
+        {
+            Assert.AreEqual(31, math.Add(20, 11));
+        }
+
+        [TestCase]
+        public void Sub()
+        {
+            Assert.AreEqual(10, math.Sub(20, 10));
+        }
+    }
+}
